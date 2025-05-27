778064abdb2a2b111dd01a44f08cb60821e93d7c
JahnaviKrishnan <jahnavi.krishnan@s.amity.edu>
2025-05-27 09:42:09 +0530

    database

--- Coffee/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoffeeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Coffee'
-def ready(self):
-        import Coffee.signals  
